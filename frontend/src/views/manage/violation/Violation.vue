8136d1f72ec941c24b8fcff7271caeb34b6be80c
fan1ke2ke <fan1ke2ke@gmail.com>
2026-03-02 16:42:01 +0800

    feat - 系统初始化

---
+++ frontend/src/views/manage/violation/Violation.vue
@@ -0,0 +1,411 @@
+<template>
+  <a-card :bordered="false" class="card-area">
+    <div :class="advanced ? 'search' : null">
+      <!-- 搜索区域 -->
+      <a-form layout="horizontal">
+        <a-row :gutter="15">
+          <div :class="advanced ? null: 'fold'">
+            <a-col :md="6" :sm="24">
+              <a-form-item
+                label="车牌号码"
+                :labelCol="{span: 5}"
+                :wrapperCol="{span: 18, offset: 1}">
+                <a-input v-model="queryParams.vehicleNo"/>
+              </a-form-item>
+            </a-col>
+            <a-col :md="6" :sm="24">
+              <a-form-item
+                label="用户名称"
+                :labelCol="{span: 5}"
+                :wrapperCol="{span: 18, offset: 1}">
+                <a-input v-model="queryParams.name"/>
+              </a-form-item>
+            </a-col>
+          </div>
+          <span style="float: right; margin-top: 3px;">
+            <a-button type="primary" @click="search">查询</a-button>
+            <a-button style="margin-left: 8px" @click="reset">重置</a-button>
+          </span>
+        </a-row>
+      </a-form>
+    </div>
+    <div>
+      <div class="operator">
+        <a-button type="primary" ghost @click="add">新增</a-button>
+        <a-button @click="batchDelete">删除</a-button>
+      </div>
+      <!-- 表格区域 -->
+      <a-table ref="TableInfo"
+               :columns="columns"
+               :rowKey="record => record.id"
+               :dataSource="dataSource"
+               :pagination="pagination"
+               :loading="loading"
+               :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+               :scroll="{ x: 900 }"
+               @change="handleTableChange">
+        <template slot="operation" slot-scope="text, record">
+          <a-icon type="file-search" @click="userViewOpen(record)" title="详 情"></a-icon>
+          <a-icon v-if="record.status == 0" type="alert" theme="twoTone" @click="userViewEdit(record)" title="处 理" style="margin-left: 15px"></a-icon>
+        </template>
+      </a-table>
+    </div>
+    <user-view
+      @close="handleuserViewClose"
+      :userShow="userView.visiable"
+      :userData="userView.data">
+    </user-view>
+    <user-edit
+      @close="handleuserEditClose"
+      @success="handleuserEditSuccess"
+      :userShow="userEdit.visiable"
+      :userData="userEdit.data">
+    </user-edit>
+    <user-add
+      v-if="bulletinAdd.visiable"
+      @close="handleBulletinAddClose"
+      @success="handleBulletinAddSuccess"
+      :bulletinAddVisiable="bulletinAdd.visiable">
+    </user-add>
+  </a-card>
+</template>
+
+<script>
+import RangeDate from '@/components/datetime/RangeDate'
+import userView from './ViolationView.vue'
+import UserAdd from './ViolationAdd.vue'
+import UserEdit from './ViolationEdit.vue'
+import {mapState} from 'vuex'
+import moment from 'moment'
+moment.locale('zh-cn')
+function getBase64 (file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = error => reject(error)
+  })
+}
+export default {
+  name: 'user',
+  components: {userView, UserAdd, UserEdit, RangeDate},
+  data () {
+    return {
+      bulletinAdd: {
+        visiable: false
+      },
+      faceView: {
+        visiable: false,
+        data: null
+      },
+      advanced: false,
+      userAdd: {
+        visiable: false
+      },
+      userEdit: {
+        visiable: false
+      },
+      userView: {
+        visiable: false,
+        data: null
+      },
+      queryParams: {},
+      filteredInfo: null,
+      sortedInfo: null,
+      paginationInfo: null,
+      dataSource: [],
+      selectedRowKeys: [],
+      loading: false,
+      pagination: {
+        pageSizeOptions: ['10', '20', '30', '40', '100'],
+        defaultCurrent: 1,
+        defaultPageSize: 10,
+        showQuickJumper: true,
+        showSizeChanger: true,
+        showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
+      },
+      fileList: [],
+      previewVisible: false,
+      previewImage: '',
+      userList: []
+    }
+  },
+  computed: {
+    ...mapState({
+      currentUser: state => state.account.user
+    }),
+    columns () {
+      return [ {
+        title: '违规编号',
+        ellipsis: true,
+        dataIndex: 'code',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '车牌号',
+        dataIndex: 'vehicleNo',
+        ellipsis: true,
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '处理状态',
+        dataIndex: 'status',
+        customRender: (text, row, index) => {
+          switch (text) {
+            case '0':
+              return <a-tag color="#f50">未处理</a-tag>
+            case '1':
+              return <a-tag color="#87d068">已处理</a-tag>
+            default:
+              return '- -'
+          }
+        }
+      }, {
+        title: '图片',
+        dataIndex: 'images',
+        customRender: (text, record, index) => {
+          if (!record.images) return <a-avatar shape="square" icon="user" />
+          return <a-popover>
+            <template slot="content">
+              <a-avatar shape="square" size={132} icon="user" src={ 'http://127.0.0.1:9527/imagesWeb/' + record.images.split(',')[0] } />
+            </template>
+            <a-avatar shape="square" icon="user" src={ 'http://127.0.0.1:9527/imagesWeb/' + record.images.split(',')[0] } />
+          </a-popover>
+        }
+      }, {
+        title: '所属用户',
+        dataIndex: 'name',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '违规内容',
+        dataIndex: 'content',
+        ellipsis: true,
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '创建时间',
+        dataIndex: 'createDate',
+        ellipsis: true,
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '操作',
+        dataIndex: 'operation',
+        scopedSlots: {customRender: 'operation'}
+      }]
+    }
+  },
+  mounted () {
+    this.fetch()
+  },
+  methods: {
+    add () {
+      this.bulletinAdd.visiable = true
+    },
+    handleBulletinAddClose () {
+      this.bulletinAdd.visiable = false
+    },
+    handleBulletinAddSuccess () {
+      this.bulletinAdd.visiable = false
+      this.$message.success('新增违规成功')
+      this.search()
+    },
+    handleCancel () {
+      this.previewVisible = false
+    },
+    async handlePreview (file) {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj)
+      }
+      this.previewImage = file.url || file.preview
+      this.previewVisible = true
+    },
+    picHandleChange (info) {
+      console.log(info.file.response)
+      if (info.file.response !== undefined && info.file.response.msg !== undefined) {
+        if (info.file.response.msg === 'success') {
+          this.$message.success('添加照片成功')
+          this.faceView.visiable = false
+          this.fetch()
+        } else {
+          this.$message.error(info.file.response.msg)
+        }
+      }
+      this.fileList = info.fileList
+    },
+    face (row) {
+      this.fileList = []
+      this.faceView.visiable = true
+      this.faceView.data = row
+    },
+    onSelectChange (selectedRowKeys) {
+      this.selectedRowKeys = selectedRowKeys
+    },
+    toggleAdvanced () {
+      this.advanced = !this.advanced
+    },
+    handleuserAddClose () {
+      this.userAdd.visiable = false
+    },
+    handleuserAddSuccess () {
+      this.userAdd.visiable = false
+      this.$message.success('新增会员成功')
+      this.search()
+    },
+    edit (record) {
+      this.$refs.userEdit.setFormValues(record)
+      this.userEdit.visiable = true
+    },
+    userViewEdit (row) {
+      this.userEdit.data = row
+      this.userEdit.visiable = true
+    },
+    userViewOpen (row) {
+      this.userView.data = row
+      this.userView.visiable = true
+    },
+    handleuserViewClose () {
+      this.userView.visiable = false
+    },
+    handleuserEditClose () {
+      this.userEdit.visiable = false
+    },
+    handleuserEditSuccess () {
+      this.userEdit.visiable = false
+      this.$message.success('修改成功')
+      this.search()
+    },
+    handleDeptChange (value) {
+      this.queryParams.deptId = value || ''
+    },
+    batchDelete () {
+      if (!this.selectedRowKeys.length) {
+        this.$message.warning('请选择需要删除的记录')
+        return
+      }
+      let that = this
+      this.$confirm({
+        title: '确定删除所选中的记录?',
+        content: '当您点击确定按钮后，这些记录将会被彻底删除',
+        centered: true,
+        onOk () {
+          let ids = that.selectedRowKeys.join(',')
+          that.$delete('/cos/violation-info/' + ids).then(() => {
+            that.$message.success('删除成功')
+            that.selectedRowKeys = []
+            that.search()
+          })
+        },
+        onCancel () {
+          that.selectedRowKeys = []
+        }
+      })
+    },
+    search () {
+      let {sortedInfo, filteredInfo} = this
+      let sortField, sortOrder
+      // 获取当前列的排序和列的过滤规则
+      if (sortedInfo) {
+        sortField = sortedInfo.field
+        sortOrder = sortedInfo.order
+      }
+      this.fetch({
+        sortField: sortField,
+        sortOrder: sortOrder,
+        ...this.queryParams,
+        ...filteredInfo
+      })
+    },
+    reset () {
+      // 取消选中
+      this.selectedRowKeys = []
+      // 重置分页
+      this.$refs.TableInfo.pagination.current = this.pagination.defaultCurrent
+      if (this.paginationInfo) {
+        this.paginationInfo.current = this.pagination.defaultCurrent
+        this.paginationInfo.pageSize = this.pagination.defaultPageSize
+      }
+      // 重置列过滤器规则
+      this.filteredInfo = null
+      // 重置列排序规则
+      this.sortedInfo = null
+      // 重置查询参数
+      this.queryParams = {}
+      this.fetch()
+    },
+    handleTableChange (pagination, filters, sorter) {
+      // 将这三个参数赋值给Vue data，用于后续使用
+      this.paginationInfo = pagination
+      this.filteredInfo = filters
+      this.sortedInfo = sorter
+
+      this.fetch({
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...this.queryParams,
+        ...filters
+      })
+    },
+    fetch (params = {}) {
+      // 显示loading
+      this.loading = true
+      if (this.paginationInfo) {
+        // 如果分页信息不为空，则设置表格当前第几页，每页条数，并设置查询分页参数
+        this.$refs.TableInfo.pagination.current = this.paginationInfo.current
+        this.$refs.TableInfo.pagination.pageSize = this.paginationInfo.pageSize
+        params.size = this.paginationInfo.pageSize
+        params.current = this.paginationInfo.current
+      } else {
+        // 如果分页信息为空，则设置为默认值
+        params.size = this.pagination.defaultPageSize
+        params.current = this.pagination.defaultCurrent
+      }
+      if (params.delFlag === undefined) {
+        delete params.delFlag
+      }
+      params.type = 3
+      this.$get('/cos/violation-info/page', {
+        ...params
+      }).then((r) => {
+        let data = r.data.data
+        const pagination = {...this.pagination}
+        pagination.total = data.total
+        this.dataSource = data.records
+        this.pagination = pagination
+        // 数据加载完毕，关闭loading
+        this.loading = false
+      })
+    }
+  },
+  watch: {}
+}
+</script>
+<style lang="less" scoped>
+@import "../../../../static/less/Common";
+</style>
---
+++ frontend/src/views/manage/violation/ViolationView.vue
@@ -0,0 +1,179 @@
+<template>
+  <a-modal v-model="show" title="违规详情" @cancel="onClose" :width="900">
+    <template slot="footer">
+      <a-button key="back" @click="onClose" type="danger">
+        关闭
+      </a-button>
+    </template>
+    <div style="font-size: 13px;font-family: SimHei" v-if="userData !== null">
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col :span="8"><b>违规编号：</b>
+          {{ userData.code ? userData.code : '- -' }}
+        </a-col>
+        <a-col :span="8"><b>车牌号：</b>
+          {{ userData.vehicleNo ? userData.vehicleNo : '- -' }}
+        </a-col>
+        <a-col :span="8"><b>车辆编号：</b>
+          {{ userData.vehicleNumber ? userData.vehicleNumber : '- -' }}
+        </a-col>
+      </a-row>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col :span="8"><b>用户编号：</b>
+          {{ userData.userCode ? userData.userCode : '- -' }}
+        </a-col>
+        <a-col :span="8"><b>用户名称：</b>
+          {{ userData.name ? userData.name : '- -' }}
+        </a-col>
+        <a-col :span="8"><b>联系方式：</b>
+          {{ userData.phone ? userData.phone : '- -' }}
+        </a-col>
+      </a-row>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col :span="8"><b>创建时间：</b>
+          {{ userData.createDate ? userData.createDate : '- -' }}
+        </a-col>
+        <a-col :span="8"><b>处理状态：</b>
+          <a-tag v-if="userData.status == 0" color="#f50">未处理</a-tag>
+          <a-tag v-if="userData.status == 1" color="#87d068">已处理</a-tag>
+        </a-col>
+      </a-row>
+      <br/>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col :span="24"><b>违规内容：</b>
+          {{ userData.content ? userData.content : '- -' }}
+        </a-col>
+      </a-row>
+      <br/>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col :span="24"><b>处理内容：</b>
+          {{ userData.fixContent ? userData.fixContent : '- -' }}
+        </a-col>
+      </a-row>
+      <br/>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">违规图片</span>
+        </a-col>
+        <a-col :span="24">
+          <a-upload
+            name="avatar"
+            action="http://127.0.0.1:9527/file/fileUpload/"
+            list-type="picture-card"
+            :file-list="fileList"
+            @preview="handlePreview"
+            @change="picHandleChange"
+          >
+          </a-upload>
+          <a-modal :visible="previewVisible" :footer="null" @cancel="handleCancel">
+            <img alt="example" style="width: 100%" :src="previewImage"/>
+          </a-modal>
+        </a-col>
+      </a-row>
+      <br/>
+    </div>
+  </a-modal>
+</template>
+
+<script>
+import baiduMap from '@/utils/map/baiduMap'
+import moment from 'moment'
+
+moment.locale('zh-cn')
+
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = error => reject(error)
+  })
+}
+
+export default {
+  name: 'userView',
+  props: {
+    userShow: {
+      type: Boolean,
+      default: false
+    },
+    userData: {
+      type: Object
+    }
+  },
+  computed: {
+    show: {
+      get: function () {
+        return this.userShow
+      },
+      set: function () {
+      }
+    }
+  },
+  data() {
+    return {
+      loading: false,
+      fileList: [],
+      previewVisible: false,
+      previewImage: '',
+      repairInfo: null,
+      durgList: [],
+      logisticsList: [],
+      userInfo: null
+    }
+  },
+  watch: {
+    userShow: function (value) {
+      if (value) {
+        if (this.userData.images) {
+          this.imagesInit(this.userData.images)
+        }
+      }
+    }
+  },
+  methods: {
+    local(userData) {
+      baiduMap.clearOverlays()
+      baiduMap.rMap().enableScrollWheelZoom(true)
+      // eslint-disable-next-line no-undef
+      let point = new BMap.Point(userData.longitude, userData.latitude)
+      baiduMap.pointAdd(point)
+      baiduMap.findPoint(point, 16)
+      // let driving = new BMap.DrivingRoute(baiduMap.rMap(), {renderOptions:{map: baiduMap.rMap(), autoViewport: true}});
+      // driving.search(new BMap.Point(this.nowPoint.lng,this.nowPoint.lat), new BMap.Point(scenic.point.split(",")[0],scenic.point.split(",")[1]));
+    },
+    imagesInit(images) {
+      if (images !== null && images !== '') {
+        let imageList = []
+        images.split(',').forEach((image, index) => {
+          imageList.push({uid: index, name: image, status: 'done', url: 'http://127.0.0.1:9527/imagesWeb/' + image})
+        })
+        this.fileList = imageList
+      }
+    },
+    handleCancel() {
+      this.previewVisible = false
+    },
+    async handlePreview(file) {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj)
+      }
+      this.previewImage = file.url || file.preview
+      this.previewVisible = true
+    },
+    picHandleChange({fileList}) {
+      this.fileList = fileList
+    },
+    onClose() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
